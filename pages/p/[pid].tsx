46c94777ae55614f2bf3a1251946f0aec09a01e1
Nat Ring <natring@rocketinsights.com>
2022-03-15 11:13:47 -0400

    Touch file to trigger redeploy

--- pages/p/[pid].tsx
+++ pages/p/[pid].tsx
@@ -16,6 +16,7 @@ const Pid = ({ cmsData }: PostOrPageProps) => {
 
 export default Pid
 
+
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   if (!(params && params.pid && Array.isArray(params.pid))) throw Error('getStaticProps: wrong parameters.')
   const [pid] = params.pid.reverse()
