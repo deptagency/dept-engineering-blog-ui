c22e6e4f445f0880d8fe6cf6650b147a1da02199
Nat Ring <natring@rocketinsights.com>
2022-03-17 11:23:15 -0400

    Remove Contact from main nav

--- appConfig.ts
+++ appConfig.ts
@@ -49,7 +49,7 @@ export const maxDepth: number = 2
 // Override: [{ label: 'Home', url: '/' }]
 // Add new: [{ label: 'Contact', url: '/contact' }]
 import { NavItem } from '@lib/ghost'
-export const customNavigation: NavItem[] = [{ label: 'Contact', url: '/contact' }]
+export const customNavigation: NavItem[] = []
 
 // Incremental Static Regenerations (ISR)
 // Note: must be disbaled, when using `next export`
