843771343ca7466396d7c88d232e9e6eb13542e3
Nat Ring <natring@rocketinsights.com>
2022-03-22 09:59:47 -0400

    Update Site URL

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 module.exports = {
   // see https://github.com/iamvishnusankar/next-sitemap
-  siteUrl: 'https://next.jamify.org',
+  siteUrl: process.env.SITE_URL || 'https://engineering.deptagency.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 }
