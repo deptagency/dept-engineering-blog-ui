8d99aee1e2003180b548e63ca1435d30b53caf3c
Nat Ring <natring@rocketinsights.com>
2022-04-08 15:28:11 -0400

    Get rid of StickyNav on Post page

--- components/Post.tsx
+++ components/Post.tsx
@@ -10,12 +10,9 @@ import { Layout } from '@components/Layout'
 import { HeaderPost } from '@components/HeaderPost'
 import { PreviewPosts } from '@components/PreviewPosts'
 import { RenderContent } from '@components/RenderContent'
-import { CommentoComments } from '@components/CommentoComments'
-import { DisqusComments } from '@components/DisqusComments'
 import { Subscribe } from '@components/Subscribe'
 import { TableOfContents } from '@components/toc/TableOfContents'
 
-import { StickyNavContainer } from '@effects/StickyNavContainer'
 import { SEO } from '@meta/seo'
 
 import { PostClass } from '@helpers/PostClass'
@@ -60,100 +57,93 @@ export const Post = ({ cmsData }: PostProps) => {
   return (
     <>
       <SEO {...{ description, settings, seoImage, article: post, title }} />
-      <StickyNavContainer
-        throttle={300}
-        isPost={true}
-        activeClass="nav-post-title-active"
-        render={(sticky) => (
-          <Layout
-            {...{ bodyClass, settings, sticky }}
-            header={<HeaderPost {...{ settings, sticky, title }} />}
-            previewPosts={<PreviewPosts {...{ settings, primaryTag: post.primary_tag, posts: previewPosts, prev: prevPost, next: nextPost }} />}
-          >
-            <div className="inner">
-              <div className="grid-wrapper">
-                <article className={`post-full grid-inner ${postClass}`}>
-                  <div className="grid-post">
-                    <header className="post-full-header post-header">
-                      {post.primary_tag && (
-                        <section className="post-full-tags">
-                          <Link href={resolveUrl({ cmsUrl, slug: post.primary_tag.slug, url: post.primary_tag.url })}>
-                            <a>{post.primary_tag.name}</a>
-                          </Link>
-                        </section>
-                      )}
-
-                      <h1 ref={sticky && sticky.anchorRef} className="post-full-title">
-                        {title}
-                      </h1>
-
-                      {post.custom_excerpt && <p className="post-full-custom-excerpt">{post.custom_excerpt}</p>}
-
-                      <div className="post-full-byline">
-                        <section className="post-full-byline-content">
-                          <section className="post-full-byline-meta">
-                            <h4 className="author-name">
-                              {post.authors?.map((author, i) => (
-                                <div key={i}>
-                                  {i > 0 ? `, ` : ``}
-                                  <Link href={resolveUrl({ cmsUrl, slug: author.slug, url: author.url || undefined })}>
-                                    <a>{author.name}</a>
-                                  </Link>
-                                </div>
-                              ))}
-                            </h4>
-                            <div className="byline-meta-content">
-                              <time className="byline-meta-date" dateTime={post.published_at || ''}>
-                                {dayjs(post.published_at || '').format('D MMMM, YYYY')}&nbsp;
-                              </time>
-                            </div>
-                          </section>
-                        </section>
-                      </div>
-                    </header>
-
-                    <section className="post-full-content post-content">
-                      <div className="post-content load-external-scripts">
-                        <RenderContent htmlAst={htmlAst} />
-                      </div>
+      <Layout
+        {...{ bodyClass, settings }}
+        header={<HeaderPost {...{ settings, title }} />}
+        previewPosts={<PreviewPosts {...{ settings, primaryTag: post.primary_tag, posts: previewPosts, prev: prevPost, next: nextPost }} />}
+      >
+        <div className="inner">
+          <div className="grid-wrapper">
+            <article className={`post-full grid-inner ${postClass}`}>
+              <div className="grid-post">
+                <header className="post-full-header post-header">
+                  {post.primary_tag && (
+                    <section className="post-full-tags">
+                      <Link href={resolveUrl({ cmsUrl, slug: post.primary_tag.slug, url: post.primary_tag.url })}>
+                        <a>{post.primary_tag.name}</a>
+                      </Link>
                     </section>
+                  )}
 
-                    <div className="post-meta">
-                      {featImg &&
-                        (nextImages.feature && featImg.dimensions ? (
-                          <figure className="post-full-image" style={{ display: 'inherit' }}>
-                            <Image
-                              src={featImg.url}
-                              alt={title}
-                              quality={nextImages.quality}
-                              layout="responsive"
-                              sizes={`
-                                    (max-width: 350px) 350px,
-                                    (max-width: 530px) 530px,
-                                    (max-width: 710px) 710px,
-                                    (max-width: 1170px) 1170px,
-                                    (max-width: 2110px) 2110px, 2000px
-                                  `}
-                              {...featImg.dimensions}
-                            />
-                          </figure>
-                        ) : (
-                          post.feature_image && (
-                            <figure className="post-full-image">
-                              <img src={post.feature_image} alt={title} />
-                            </figure>
-                          )
-                        ))}
-                      {toc.enable && !!post.toc && <TableOfContents {...{ toc: post.toc, url: resolveUrl({ cmsUrl, collectionPath, slug, url }), maxDepth: toc.maxDepth, lang }} />}
-                    </div>
+                  <h1 className="post-full-title">
+                    {title}
+                  </h1>
+
+                  {post.custom_excerpt && <p className="post-full-custom-excerpt">{post.custom_excerpt}</p>}
+
+                  <div className="post-full-byline">
+                    <section className="post-full-byline-content">
+                      <section className="post-full-byline-meta">
+                        <h4 className="author-name">
+                          {post.authors?.map((author, i) => (
+                            <div key={i}>
+                              {i > 0 ? `, ` : ``}
+                              <Link href={resolveUrl({ cmsUrl, slug: author.slug, url: author.url || undefined })}>
+                                <a>{author.name}</a>
+                              </Link>
+                            </div>
+                          ))}
+                        </h4>
+                        <div className="byline-meta-content">
+                          <time className="byline-meta-date" dateTime={post.published_at || ''}>
+                            {dayjs(post.published_at || '').format('D MMMM, YYYY')}&nbsp;
+                          </time>
+                        </div>
+                      </section>
+                    </section>
                   </div>
-                </article>
+                </header>
+
+                <section className="post-full-content post-content">
+                  <div className="post-content load-external-scripts">
+                    <RenderContent htmlAst={htmlAst} />
+                  </div>
+                </section>
+
+                <div className="post-meta">
+                  {featImg &&
+                    (nextImages.feature && featImg.dimensions ? (
+                      <figure className="post-full-image" style={{ display: 'inherit' }}>
+                        <Image
+                          src={featImg.url}
+                          alt={title}
+                          quality={nextImages.quality}
+                          layout="responsive"
+                          sizes={`
+                                (max-width: 350px) 350px,
+                                (max-width: 530px) 530px,
+                                (max-width: 710px) 710px,
+                                (max-width: 1170px) 1170px,
+                                (max-width: 2110px) 2110px, 2000px
+                              `}
+                          {...featImg.dimensions}
+                        />
+                      </figure>
+                    ) : (
+                      post.feature_image && (
+                        <figure className="post-full-image">
+                          <img src={post.feature_image} alt={title} />
+                        </figure>
+                      )
+                    ))}
+                  {toc.enable && !!post.toc && <TableOfContents {...{ toc: post.toc, url: resolveUrl({ cmsUrl, collectionPath, slug, url }), maxDepth: toc.maxDepth, lang }} />}
+                </div>
               </div>
-            </div>
-            {memberSubscriptions && <Subscribe {...{ settings }} />}
-          </Layout>
-        )}
-      />
+            </article>
+          </div>
+        </div>
+        {memberSubscriptions && <Subscribe {...{ settings }} />}
+      </Layout>
     </>
   )
 }
