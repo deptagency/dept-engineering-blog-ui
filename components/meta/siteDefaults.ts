437ae31e4f8649558b13e980d160bd72b1600ee5
Chantal Deaton <65011231+cmdea@users.noreply.github.com>
2022-05-17 16:58:28 -0400

    Fix prettier error (#67)

--- components/meta/siteDefaults.ts
+++ components/meta/siteDefaults.ts
@@ -1,6 +1,7 @@
 // Defaults for meta, if not configured in CMS
 export const siteTitleMeta = 'DEPT® Engineering Blog'
-export const siteDescriptionMeta = 'Ideas & insights from the DEPT® developer community'
+export const siteDescriptionMeta =
+  'Ideas & insights from the DEPT® developer community'
 
 // Defaults, if not configured in CMS
 // Images can be fund in the /public folder
