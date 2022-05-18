998f0802b8eca7df600487fc093af46be37ca4e7
Chantal Deaton <chantal.deaton@deptagency.com>
2022-05-17 20:08:14 -0400

    improve readability of css

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import { ElementType } from 'react'
+import { css } from '@emotion/react'
 
 import { colors } from '@components/common/colors'
 import { spaces } from '@components/common/spaces'
@@ -29,153 +30,122 @@ const mapVariantToStyles = ({
   disabled,
   selected
 }: ButtonCtaProps &
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ButtonTagProps & { variant?: ButtonVariant } & { as?: ElementType<any> }) => {
+  ButtonTagProps & { variant?: ButtonVariant } & {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    as?: ElementType<any>
+  }) => {
   const hasHoverState =
     (as === 'button' || as === 'a' || as === undefined) && !disabled
 
   switch (variant) {
     case 'tag':
-      return `
+      return css`
         font-weight: 500;
         background: ${colors.platinum};
         border: 1px solid ${colors.platinum};
         color: ${colors.onyx};
 
-        ${
-          small
-            ? `
-          font-size: 1.2rem;
-          line-height: 1.2rem;
-          padding: ${spaces.xxs - 2}px ${spaces.xxs}px;
-        `
-            : ``
-        }
-
-        ${
-          white
-            ? `
+        ${white &&
+        css`
           font-weight: 700;
           background: ${colors.white};
           border: 1px solid ${colors.onyx};
           color: ${colors.onyx};
+        `}
 
-          ${
-            selected
-              ? `
-            background: ${colors.onyx};
-            color: ${colors.white};
-          `
-              : ``
-          }
-        `
-            : ``
-        }
+        ${selected &&
+        white &&
+        css`
+          background: ${colors.onyx};
+          color: ${colors.white};
+        `}
 
-        ${
-          disabled
-            ? `
+        ${small &&
+        css`
+          font-size: 1.2rem;
+          line-height: 1.2rem;
+          padding: ${spaces.xxs - 2}px ${spaces.xxs}px;
+        `}
+
+        ${disabled &&
+        css`
           cursor: default;
           opacity: 0.5;
-        `
-            : ``
-        }
+        `}
 
-        ${
-          hasHoverState
-            ? `
+        ${hasHoverState &&
+        css`
           &:hover {
             cursor: pointer;
 
-            ${
-              white
-                ? `
+            ${white &&
+            css`
               background: ${colors.onyx};
               color: ${colors.white};
+            `}
 
-              ${
-                selected
-                  ? `
-                background: ${colors.white};
-                color: ${colors.onyx};
-              `
-                  : ``
-              }
-            `
-                : ``
-            }
+            ${selected &&
+            white &&
+            css`
+              background: ${colors.white};
+              color: ${colors.onyx};
+            `}
           }
-        `
-            : ``
-        }
+        `}
       `
     default:
-      return `
+      return css`
         border-radius: 100rem;
         border: 1px solid ${colors.onyx};
         background: ${colors.onyx};
         color: ${colors.white};
 
-        ${
-          small
-            ? `
-          font-size: 1.2rem;
-          line-height: 1.2rem;
-          padding: ${spaces.xxs - 2}px ${spaces.xxs}px;
-        `
-            : ``
-        }
-
-        ${
-          inverted
-            ? `
+        ${inverted &&
+        css`
           border: 1px solid ${colors.white};
           background: ${colors.onyx};
           color: ${colors.white};
-        `
-            : ``
-        }
+        `}
 
-        ${
-          disabled
-            ? `
+        ${small &&
+        css`
+          font-size: 1.2rem;
+          line-height: 1.2rem;
+          padding: ${spaces.xxs - 2}px ${spaces.xxs}px;
+        `}
+
+        ${disabled &&
+        css`
           cursor: default;
           opacity: 0.5;
-        `
-            : ``
-        }
+        `}
 
-        ${
-          hasHoverState
-            ? `
+        ${hasHoverState &&
+        css`
           &:hover {
+            cursor: pointer;
             background: ${colors.white};
             color: ${colors.onyx};
-            cursor: pointer;
 
-            ${
-              inverted
-                ? `
+            ${inverted &&
+            css`
               background: ${colors.white};
               color: ${colors.onyx};
-            `
-                : ``
-            }
+            `}
           }
-        `
-            : ``
-        }
+        `}
       `
   }
 }
 
-const COMMON_STYLES = `
+const COMMON_STYLES = css`
   display: inline-block;
   padding: ${spaces.xxs}px ${spaces.sm}px;
   font-size: 1.4rem;
   font-weight: 700;
   line-height: 2rem;
   text-transform: uppercase;
+  cursor: default;
 
   &:hover {
     text-decoration: none;
