4524053fc64e7ff49e9f4796cb663ceeaaa7b68d
Nat Ring <natring@rocketinsights.com>
2023-02-06 11:34:00 -0500

    Codemon to update next/image references, fix resulting linting issues

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from 'next/image'
+import Image from 'next/legacy/image'
 
 import { GhostPostOrPage, GhostSettings } from '@lib/ghost'
 
