9ab19b4a99641b9d2762825d08f6d1f351f4aeb5
Nat Ring <natring@rocketinsights.com>
2023-10-12 11:33:10 -0400

    Add staging URL to list of image domains

--- next.config.js
+++ next.config.js
@@ -24,6 +24,7 @@ const defaultOptions = withBundleAnalyzer({
       'repository-images.githubusercontent.com',
       'www.gravatar.com',
       'github.githubassets.com',
+      'dept-engineering-blog-staging.ghost.io',
       ...(process.env.IMAGE_DOMAINS || '').split(',')
     ]
   },
